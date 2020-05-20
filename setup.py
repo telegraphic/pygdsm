ad28375d334607fedef618ec8713bf34b695f93e
Danny Price <dan@thetelegraphic.com>
2020-05-20 22:39:32 +1000

    Updates for .travis.yml

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 """A setuptools based setup module.
-
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
@@ -23,17 +19,17 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.0.0',
+    version='1.0.0',
 
     description='Python Global Sky Model of diffuse Galactic radio emission',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/telegraphic/PyGSM',
+    url='https://github.com/telegraphic/pygdsm',
 
     # Author details
     author='Danny Price',
-    author_email='dprice@cfa.harvard.edu',
+    author_email='dancpr@berkeley.edu',
 
     # Choose your license
     license='MIT',
@@ -56,12 +52,10 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
@@ -84,18 +78,6 @@ setup(
     extras_require={
     },
 
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={
-        'pygdsm': ['gsm_components.h5', 'gsm2016_components.h5'],
-    },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
