fe7f698cfa9d7816ae91b26d295abebdf7b7d234
Danny Price <dan@thetelegraphic.com>
2021-07-14 17:12:50 +0800

    Adding tests for Haslam

---
+++ tests/test_haslam.py
@@ -0,0 +1,46 @@
+import sys
+
+sys.path.append("/workdata/pygdsm")
+
+import pylab as plt
+import healpy as hp
+from datetime import datetime
+
+from pygdsm import HaslamSkyModel, HaslamObserver, GSMObserver
+
+def test_compare_gsm_to_old():
+
+    gl = HaslamSkyModel(freq_unit='MHz')
+    dl = gl.generate(408)
+    gl.view()
+
+    import pylab as plt
+    plt.show()
+
+
+def test_observer_test():
+
+    # Setup observatory location - in this case, Parkes Australia
+    (latitude, longitude, elevation) = ('-32.998370', '148.263659', 100)
+    ov = HaslamObserver()
+    ov.lon = longitude
+    ov.lat = latitude
+    ov.elev = elevation
+    ov.date = datetime(2000, 1, 1, 23, 0)
+
+    ov.generate(200)
+    d = ov.view(logged=True)
+
+    ov = GSMObserver()
+    ov.lon = longitude
+    ov.lat = latitude
+    ov.elev = elevation
+    ov.date = datetime(2000, 1, 1, 23, 0)
+
+    ov.generate(200)
+    d = ov.view(logged=True)
+    plt.show()
+
+if __name__ == "__main__":
+    test_compare_gsm_to_old()
+    test_observer_test()
