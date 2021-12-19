39bbb1a7c132880ffbc794431f277d24f7a9e47b
Danny Price <dan@thetelegraphic.com>
2021-12-19 11:01:44 +0800

    Set remote_timeout to 30 seconds for zenodo download

--- pygdsm/component_data.py
+++ pygdsm/component_data.py
@@ -1,7 +1,11 @@
-from astropy.utils.data import download_file
+from astropy.utils.data import conf, download_file
+
+# Zenodo can be slow to respond, so set timeout to 30 seconds
+conf.remote_timeout = 30
 
 GSM_FILEPATH      = download_file('https://zenodo.org/record/3835582/files/gsm_components.h5?download=1',
                                   cache=True, show_progress=True)
+
 GSM2016_FILEPATH  = download_file('https://zenodo.org/record/3835582/files/gsm2016_components.h5?download=1',
                                   cache=True, show_progress=True)
 
