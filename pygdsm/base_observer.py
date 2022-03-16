ef9872593b92cf6c57874b698b1ac5682d36b594
cdilullo <cdilullo@unm.edu>
2022-03-16 13:07:34 -0400

    Reworked how horizon masking was computed.

--- pygdsm/base_observer.py
+++ pygdsm/base_observer.py
@@ -53,24 +53,36 @@ class BaseObserver(ephem.Observer):
         sky = self.gsm.generated_map_data
 
         # Get RA and DEC of zenith
-        ra_rad, dec_rad = self.radec_of(0, np.pi/2)
-        ra_deg  = ra_rad / np.pi * 180
-        dec_deg = dec_rad / np.pi * 180
+        ra_zen, dec_zen = self.radec_of(0, np.pi/2)
+        ra_zen  *= (180 / np.pi)
+        dec_zen *= (180 / np.pi)
 
+        # Transform from Galactic coordinates to Equatorial
+        rot = hp.Rotator(coord=['G', 'C'])
+        eq_theta, eq_phi = rot(self._theta, self._phi)
 
-        # Apply rotation
-        hrot = hp.Rotator(rot=[ra_deg, dec_deg], coord=['G', 'C'], inv=True)
+        # Convert from Equatorial colatitude and longitude to normal RA and DEC
+        dec = 90.0 - np.abs(eq_theta*(180/np.pi))
+        ra = ( (eq_phi + 2*np.pi) % (2*np.pi) )*(180/np.pi)
+
+        # Generate a mask for below horizon (distance from zenith > 90 deg)
+        dist = 2 * np.arcsin( np.sqrt( np.sin((dec_zen-dec)*(np.pi/180)/2)**2 + \
+                np.cos(dec_zen*(np.pi/180))*np.cos(dec*(np.pi/180))*np.sin((ra_zen-ra)*(np.pi/180)/2)**2 ) )
+        mask = dist > (np.pi/2)
+
+        # Apply rotation to convert from Galactic to Equatorial and center on zenith
+        hrot = hp.Rotator(rot=[ra_zen, dec_zen], coord=['G', 'C'], inv=True)
         g0, g1 = hrot(self._theta, self._phi)
         pix0 = hp.ang2pix(self._n_side, g0, g1)
         sky_rotated = sky[pix0]
-
-        # Generate a mask for below horizon
-        mask1 = self._theta + np.pi / 2 > 2 * np.pi
-        mask2 = self._theta < np.pi / 2
-        mask = np.invert(np.logical_or(mask1, mask2))
+        mask_rotated = mask[pix0]
+        dec_rotated = dec[pix0]
+        ra_rotated = ra[pix0]
 
         self.observed_sky = hp.ma(sky_rotated)
-        self.observed_sky.mask = mask
+        self.observed_sky.mask = mask_rotated
+        self._observed_ra = ra_rotated
+        self._observed_dec = dec_rotated
 
         return self.observed_sky
 
