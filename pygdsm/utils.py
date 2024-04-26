84991f9a04b0c8d99833ec5fc2c3c47de66238b0
Danny Price <danny.price@skao.int>
2024-04-26 10:42:53 +0800

    Adding missing utils.py

---
+++ pygdsm/utils.py
@@ -0,0 +1,33 @@
+import numpy as np
+import healpy as hp
+from astropy.coordinates import SkyCoord
+
+
+def hpix2sky(nside: int, pix_ids: np.ndarray) -> SkyCoord:
+    """ Convert a healpix pixel_id into a SkyCoord
+
+    Args:
+        nside (int): Healpix NSIDE parameter
+        pix_ids (np.array): Array of pixel IDs
+
+    Returns:
+        sc (SkyCoord): Corresponding SkyCoordinates
+    """
+    gl, gb = hp.pix2ang(nside, pix_ids, lonlat=True)
+    sc = SkyCoord(gl, gb, frame='galactic', unit=('deg', 'deg'))
+    return sc
+
+
+def sky2hpix(nside: int, sc: SkyCoord) -> np.ndarray:
+    """ Convert a SkyCoord into a healpix pixel_id
+
+    Args:
+        nside (int): Healpix NSIDE parameter
+        sc (SkyCoord): Astropy sky coordinates array
+
+    Returns:
+        pix (np.array): Array of healpix pixel IDs
+    """
+    gl, gb = sc.galactic.l.to('deg').value, sc.galactic.b.to('deg').value
+    pix = hp.ang2pix(nside, gl, gb, lonlat=True)
+    return pix
---
+++ tests/test_utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+import healpy as hp
+from pygdsm.utils import hpix2sky, sky2hpix
+
+
+def test_pix2sky():
+    """ Small test routine for converting healpix pixel_id to and from SkyCoords """
+    NSIDE = 32
+    pix = np.arange(hp.nside2npix(NSIDE))
+    sc  = hpix2sky(NSIDE, pix)
+    pix_roundtrip = sky2hpix(NSIDE, sc)
+    assert np.allclose(pix, pix_roundtrip)
+
+
+if __name__ == "__main__":
+    test_pix2sky()
